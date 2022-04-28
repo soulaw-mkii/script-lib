2190fc9b3a331979f03386e93e24fe326fdd5abe
soulaw-mkii <steveyau@hotmail.com>
2022-04-29 01:27:51 +0800

    Add subroutine to init env var and k8s context

--- _functions.sh
+++ _functions.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+init_env() {
+  echo `basename "$0"`
+  IFS='._ ' read -ra arr <<< `basename "$0"`
+  export ENV="${arr[1]}"
+  export NAMESPACE="${arr[2]}"
+  export K8S_CONTEXT="${ENV}-${NAMESPACE}"
+}
+
+init_k8s_ctx() {
+  log_msg "Switch to $K8S_CONTEXT..."
+  kubectl config use-context $K8S_CONTEXT
+
+  log_msg "Create namespace $NAMESPACE if not exist..."
+  kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
+}
+
 log_msg () {
   echo -e "\n\n[`date "+%Y/%m/%d %H:%M:%S"`] $1\n"
 }
